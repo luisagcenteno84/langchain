a00e7ba14aec195c327cce1ef46045782d012824
luisagcenteno84 <luisagcenteno@gmail.com>
2024-04-14 16:15:40 -0700

    renamed chatbot with history_aware retriever

--- history_aware_retriever.py
+++ history_aware_retriever.py
@@ -30,15 +30,19 @@ vector = FAISS.from_documents(documents, embeddings)
 retriever = vector.as_retriever()
 
 
+#adding a prompt that includes the history we want the model to remember
 prompt = ChatPromptTemplate.from_messages([
     MessagesPlaceholder(variable_name="chat_history"),
     ("user","{input}. \nGiven the above conversation, generate a search query to look up to get information relevant to this conversation")
 ])
 
+
+#creating a history-aware retriever from the llm, vector and prompt
 retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
 
 chat_history = [HumanMessage(content="Can LangSmith help test my LLM Applications?"),AIMessage(content="Yes!")]
 
+#retrieves the question remembering history
 retriever_chain.invoke({
      "chat_history": chat_history,
     "input":"Tell me how"
@@ -47,18 +51,23 @@ retriever_chain.invoke({
 
 
 
+#new prompt that includes the context from the documents
 prompt = ChatPromptTemplate.from_messages([
     ("system","Answer the user's questions based on the below context: \n\n{context}"),
     MessagesPlaceholder(variable_name="chat_history"),
     ("user","{input}"),
 ])
 
+#creating the documents chain
 document_chain = create_stuff_documents_chain(llm,prompt)
 
+#creating a retrieval chain from the history-aware retriever and document chain with context
 retrieval_chain = create_retrieval_chain(retriever_chain, document_chain)
 
+#simluating chat history
 chat_history = [HumanMessage(content="Can Langchain help test my LLM applications?"),AIMessage(content="Yes!")]
 
+#invoking using chat history
 response = retrieval_chain.invoke({
         "chat_history":chat_history,
         "input":"Tell me how"
